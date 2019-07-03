4033245cb568329365985089c7528b66229b3cd3
Jaypatel07 <0107.patel.jay@gmail.com>
2019-07-03 16:05:36 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using portfolio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("/projects")]
+        public IActionResult Projects()
+        {
+            return View("Projects");
+        }
+        [HttpGet("/about")]
+        public IActionResult About()
+        {
+            return View("About");
+        }
+        [HttpGet("/contact")]
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
